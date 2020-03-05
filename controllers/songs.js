b4e073b2da83aff34c2b4d616eebc3be285865de
Connor Reese <connor.reese13@gmail.com>
2020-03-05 15:29:09 +0800

    added filters

--- controllers/songs.js
+++ controllers/songs.js
@@ -1,6 +1,28 @@
-const router = require('express').Router()
-const db = require('../db')
+const router = require("express").Router();
+const db = require("../db");
 
-router.get('/', (req, res) => {})
+router.get("/", (req, res) => {
+  db.query(
+    `SELECT songs.name, albums.name AS 'album', artists.name AS 'artist', genres.name AS 'genre', songs.audio FROM songs
+INNER JOIN albums
+ON songs.album = albums.id
+INNER JOIN artists
+ON songs.artist = artists.id
+INNER JOIN genres
+ON songs.genre = genres.id
+${
+  Object.keys(req.query).length > 0
+    ? "  WHERE  " +
+      Object.keys(req.query)
+        .map(e => `  songs.${e} = ${req.query[e]}  `)
+        .join("AND")
+    : ""
+} `,
 
-module.exports = router
+    (error, results) => {
+      res.send(results ? results : error);
+    }
+  );
+});
+
+module.exports = router;
