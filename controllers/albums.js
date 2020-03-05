1b7a5b42c39bde7d232e86f6c0c2d3f686e07ca3
Connor Reese <connor.reese13@gmail.com>
2020-03-05 18:50:35 +0800

    fixed stuff

--- controllers/albums.js
+++ controllers/albums.js
@@ -2,14 +2,22 @@ const router = require("express").Router();
 const db = require("../db");
 
 router.get("/", (req, res) => {
-  db.query(`SELECT * FROM albums`, (error, results) => {
-    res.send(results ? results : error);
-  });
+  db.query(
+    `SELECT albums.id, albums.name, artists.name AS 'artist', albums.cover FROM albums
+LEFT JOIN artists
+ON albums.artist = artists.id
+`,
+    (error, results) => {
+      res.send(results ? results : error);
+    }
+  );
 });
 
 router.get("/:id", (req, res) => {
   db.query(
-    `SELECT * FROM albums WHERE id = ${req.params.id}`,
+    `SELECT albums.id, albums.name, artists.name AS 'artist', albums.cover FROM albums
+LEFT JOIN artists
+ON albums.artist = artists.id WHERE albums.id = ${req.params.id}`,
     (error, results) => {
       res.send(results ? results : error);
     }
