aaf7823fa499dd4e23c9fe7da9a2770a7933c08a
Connor Reese <connor.reese13@gmail.com>
2020-03-09 11:36:18 +0800

    added stripe

--- controllers/pay.js
+++ controllers/pay.js
@@ -1,5 +1,21 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const stripe = require("stripe")(`${process.env.STRIPE_SK}`);
 
-router.post('/', (req, res) => {})
+router.post("/", (req, res) => {
+  console.log(req.body);
+  stripe.charges
+    .create({
+      amount: 100,
+      currency: "usd",
+      description: "",
+      source: req.body.token.id
+    })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.send(err);
+    });
+});
 
-module.exports = router
+module.exports = router;
